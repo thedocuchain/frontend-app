a6d56c73fc92e09e562714e34b251b126a9b1282
Alisa Savorskaya <savorskaya.alisa@gmail.com>
2024-06-17 10:03:54 +0300

    done subscribeDocument

--- src/store/reducers/document/actions/subscribe.ts
+++ src/store/reducers/document/actions/subscribe.ts
@@ -2,15 +2,24 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 
 import { DefaultApiResponse, SuccessApiResponse } from 'src/store/reducers/types'
 import { api } from 'src/store/apis'
+import { getDocument } from 'src/store/reducers/document/actions/get-document'
 
-export const subscribeUser = createAsyncThunk(
-  'user/subscribe',
-  async (payload: { documentId: string; userEmail: string }): Promise<SuccessApiResponse | DefaultApiResponse> => {
+export const subscribeDocument = createAsyncThunk(
+  'document/subscribe',
+  async (
+    payload: { documentId: string; userEmail: string },
+    thunkAPI,
+  ): Promise<SuccessApiResponse | DefaultApiResponse> => {
     try {
-      const { data } = await api.post(`/v1/users/subscribe`, {
-        documentId: payload.documentId,
-        userEmail: payload.userEmail,
+      const { data } = await api.post(`/v1/documents/${payload.documentId}/subscribe`, {
+        email: payload.userEmail,
       })
+
+      await thunkAPI.dispatch(
+        getDocument({
+          id: payload.documentId,
+        }),
+      )
 
       return data
     } catch (ignore) {
