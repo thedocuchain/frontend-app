823752d65722dc8caf92a01b3621bdfa8cef6c06
Alisa Savorskaya <savorskaya.alisa@gmail.com>
2024-06-27 15:09:19 +0300

    added index page

---
+++ src/pages/index/index.p.tsx
@@ -0,0 +1,13 @@
+import { PageHead } from 'src/components/common/page-head'
+
+export default function IndexPage() {
+  const title = 'Index'
+
+  return (
+    <>
+      <PageHead>{title}</PageHead>
+
+      <div>index</div>
+    </>
+  )
+}
