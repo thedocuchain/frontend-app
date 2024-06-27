6e2e901a21ff1d19f8c305ad47c1d584407cb27d
Alisa Savorskaya <savorskaya.alisa@gmail.com>
2024-06-27 13:12:23 +0300

    small fixes

--- src/pages/_app.p.tsx
+++ src/pages/_app.p.tsx
@@ -62,10 +62,10 @@ export default function MyCustomApp({ Component, pageProps, initialState }: AppP
         <link rel='icon' type='image/png' sizes='16x16' href='/app/icons/favicon-16x16.png' />
         <link rel='mask-icon' href='/app/icons/safari-pinned-tab.svg' color='#5bbad5' />
         <link rel='manifest' href='/app/site.webmanifest' />
+        <link rel='manifest' href='/app/manifest.json' />
         <meta name='msapplication-TileColor' content='#da532c' />
         <meta name='theme-color' content='#ffffff' />
         <meta name='color-scheme' content='only light' />
-        <link rel='manifest' href='/app/manifest.json' />
       </Head>
       <Provider store={store}>
         <Metrics />
