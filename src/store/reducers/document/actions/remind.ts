f82d62d334e8ea4fae221ca5b75395401611b23c
Dima <dbaukov@docuchain.io>
2025-11-10 15:19:14 +0300

    add captcha to user reminder

--- src/store/reducers/document/actions/remind.ts
+++ src/store/reducers/document/actions/remind.ts
@@ -3,6 +3,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 import { DefaultApiResponse, SuccessApiResponse } from 'src/store/reducers/types'
 import { api } from 'src/store/apis'
 import { getDocument } from 'src/store/reducers/document/actions/get-document'
+import { getRecaptchaToken } from './recaptcha'
 
 export const remindUser = createAsyncThunk(
   'user/remind',
@@ -11,7 +12,11 @@ export const remindUser = createAsyncThunk(
     thunkAPI,
   ): Promise<SuccessApiResponse | DefaultApiResponse> => {
     try {
-      const { data } = await api.post(`/v1/documents/${payload.documentId}/users/${payload.userId}/notify`)
+      const { captchaToken } = await thunkAPI.dispatch(getRecaptchaToken('send_document')).unwrap()
+
+      const { data } = await api.post(`/v1/documents/${payload.documentId}/users/${payload.userId}/notify`, {
+        recaptchaToken: captchaToken,
+      })
 
       await thunkAPI.dispatch(
         getDocument({
